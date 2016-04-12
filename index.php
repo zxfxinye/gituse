8e3f2144f2a5115873d3b763feeb973f11d0ae5d
fenghe415 <fenghe415@163.com>
2016-04-12 02:29:15 +0000

    after tags

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	echo phpinfo();
 	echo 4;
-	echo 3;
+	echo 311;
 	echo 12778888;
 	echo 235;
 	echo 122;
-	echo "dsajdsajkd";
+	echo "dsajdjakdajdjaksdkadajsdsajkd";
 	echo 3333;
 	echo 4433;
 
